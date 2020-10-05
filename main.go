0cc196bf731f99241231e145319caaf4ee999b0d
Daniel Lwetabe <acehanks@gmail.com>
2020-10-05 19:17:17 +0300

    simple http server

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("starting application on PORT 9001")
+	http.HandleFunc("/", HelloServer)
+	log.Fatal(http.ListenAndServe(":9001", nil))
+}
+
+// HelloServer responds with a simple static message
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Server")
 }
