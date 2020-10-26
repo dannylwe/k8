7344e326382a9dd939163b5df3fcd788fda76757
Daniel Lwetabe <acehanks@gmail.com>
2020-10-27 00:38:24 +0300

    Added redis database

--- servicemesh/get-all-orders-api/go.mod
+++ servicemesh/get-all-orders-api/go.mod
@@ -3,6 +3,7 @@ module github.com/danny/services
 go 1.14
 
 require (
+	github.com/go-redis/redis/v8 v8.3.2
 	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.7.0
 )
--- servicemesh/get-all-orders-api/main.go
+++ servicemesh/get-all-orders-api/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
 	logger "github.com/sirupsen/logrus"
 )
+
+var ctx = context.Background()
 
 type order struct {
 	OrderID    int    `json:"orderID"`
@@ -35,12 +40,26 @@ func main() {
 
 func getAllOrders(w http.ResponseWriter, r *http.Request) {
 	orders := []order{{4, "3456", 4, 7}, {5, "3678", 1, 1}, {6, "3567", 2, 3}}
-	response, _ := json.Marshal(orders)
+	records, err := getFromRedis(ctx, "records")
 
-	logger.Info("response get all orders")
+	if err != nil {
+		stringOrders, _ := json.Marshal(orders)
+		stored := string(stringOrders)
+		saveToRedis(ctx, "records", stored)
+
+		logger.Info("response get all orders: static")
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		response, _ := json.Marshal(orders)
+		w.Write(response)
+		return
+	}
+
+	logger.Info("response get all orders: redis")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(response)
+	w.Write([]byte(records))
 }
 
 func healthCheck() http.Handler {
@@ -77,4 +96,44 @@ func CORS(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 		return
 	})
+}
+
+// redisClient creates a connection to the redis database
+func redisClient(ctx context.Context) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err := client.Ping(ctx).Result()
+
+	if err != nil {
+		logger.Error("can not connect to redis database")
+		panic(err)
+	}
+	return client
+}
+
+// saveToRedis saves key-value pair to redis
+func saveToRedis(ctx context.Context, key string, data string) error {
+	rClient := redisClient(ctx)
+	err := rClient.Set(ctx, key, data, 20*time.Second).Err()
+	if err != nil {
+		logger.Error("Failed to save to redis")
+		panic(err)
+	}
+	logger.Info("saved " + key + " to redis")
+	return err
+}
+
+// gets key-value pair from redis
+func getFromRedis(ctx context.Context, key string) (string, error) {
+	rClient := redisClient(ctx)
+	result, err := rClient.Get(ctx, key).Result()
+	if err != nil {
+		logger.Error("Failed to read from redis")
+		return result, err
+	}
+	logger.Info("got " + key + " from redis")
+	return result, nil
 }
